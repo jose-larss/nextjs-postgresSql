d9b04696d5755691e9297dc31e83d2d8f7e57b90
jlcastro <joselucianocastro10@gmail.com>
2024-02-08 22:55:19 +0100

    implemento 3 cambios

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 import '@/app/ui/global.css';
 import { monserrat } from '@/app/ui/fonts';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({children,}: {children: React.ReactNode;}) {
   return (
     <html lang="en">
-      <body className={`${monserrat.className} antialiased`}>{children}</body>
+      <body className={`${monserrat.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,12 @@ export default function Page() {
       <div className="{styles.shape} flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         { <AcmeLogo /> }
       </div>
+
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         {/* Aqui se usa un estilo modular styles */}
-        <div className={styles.shape} />
+          <div className={styles.shape} />
 
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
@@ -24,6 +26,7 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
+
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
@@ -31,6 +34,7 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
+
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
